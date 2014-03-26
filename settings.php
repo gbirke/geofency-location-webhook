e5e0b3250630e7627d9b30631dee62b3b3c62a5c
Gabriel Birke <gb@birke-software.de>
2014-03-26 17:07:51 +0100

    Added empty line for better readability

--- settings.php
+++ settings.php
@@ -8,6 +8,7 @@ define("DATA_FILE", __DIR__."/data.json");
 
 // Number of seconds that must pass from the last "exit" notice until the state is "present" 
 define("ENTER_THRESHOLD", 0);
+
 // Number of seconds that must from the last "enter" notice until the state is "absent" 
 // Use this if you have an iBeacon where the signal is lost and recovered regularly
 define("EXIT_THRESHOLD", 300); // 10 Minutes
